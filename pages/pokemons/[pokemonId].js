fa190792bc5bb39f135cc07e6e245179b1f1cb3e
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-04-21 13:36:54 -0300

    Jsx da pagina de pokemon individual

--- pages/pokemons/[pokemonId].js
+++ pages/pokemons/[pokemonId].js
@@ -1,3 +1,7 @@
+import Image from "next/image"
+
+import styles from '../../styles/Pokemon.module.css'
+
 export const getStaticPaths = async () => {
 
   const maxPokemons = 52
@@ -36,9 +40,37 @@ export const getStaticProps = async (context) => {
 
 const Pokemon = ({ pokemon }) => {
   return (
-    <>
+    <div>
       <h1>{pokemon.name}</h1>
-    </>
+      <Image 
+        src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} 
+        width={200} 
+        height={200} 
+        alt={pokemon.name} 
+      />
+      <div>
+        <h3>Número:</h3>
+        <p>#{pokemon.id}</p>
+      </div>
+      <div>
+        <h3>Tipo:</h3>
+        <div>
+          {pokemon.types.map((item, index) => (
+            <span key={index}>{item.type.name}</span>
+          ))}
+        </div>
+      </div>
+      <div>
+        <div>
+          <h4>Altura:</h4>
+          <p>{pokemon.height * 10} cm</p>
+        </div>
+        <div>
+          <h4>Peso:</h4>
+          <p>{pokemon.weight / 10} kg</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
