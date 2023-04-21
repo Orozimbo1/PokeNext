a9f7e7f97a7f3840b5a1a1237782583539fb6655
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-04-21 10:21:34 -0300

    Criando a pagina about e estilizando ela

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import styles from '../styles/About.module.css'
+
+import Image from "next/image"
+
+const About = () => {
+  return (
+    <div className={styles.about}>
+      <h1>Sobre o projeto</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dignissimos harum architecto quaerat quasi tenetur, inventore earum a asperiores autem officiis cumque ex consequatur, repudiandae vero nostrum sunt adipisci odio?</p>
+      <Image src='/images/charizard.png' width={300} height={300} alt='Charizard' />
+    </div>
+  )
+}
+
+export default About
