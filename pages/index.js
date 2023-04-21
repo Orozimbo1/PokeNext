547b3d4f400b7380bae7beeb23b9dfbd8ba48bdb
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-04-21 10:34:57 -0300

    Chamando pokemons pela api

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
 import styles from '@/styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+
+  const maxPokemons = 251
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  // Add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  })
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  }
+
+}
+
+export default function Home({ pokemons }) {
   return (
-    <>
-      <main>
-        <h1>PokeNext</h1>
-      </main>
-    </>
+    <div>
+      <h1>PokeNext</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            <h2>{pokemon.name}</h2>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
