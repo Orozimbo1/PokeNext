ad1258d4f8132f329edcdea0000e44772ea1cf2f
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-04-22 21:10:19 -0300

    Mudando o icone de pokemon

--- components/Card.js
+++ components/Card.js
@@ -8,7 +8,7 @@ const Card = ({ pokemon }) => {
   return (
     <div className={styles.card}>
       <Image 
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/diamond-pearl/${pokemon.id}.png`} 
+        src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} 
         width={100} 
         height={100} 
         alt={pokemon.name}
